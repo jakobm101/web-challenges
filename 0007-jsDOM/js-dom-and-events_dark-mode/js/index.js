069f540954f2017e0fe0d6ce0a26a402257839e0
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-05-30 10:10:40 +0200

    button light and dark are working

--- 0007-jsDOM/js-dom-and-events_dark-mode/js/index.js
+++ 0007-jsDOM/js-dom-and-events_dark-mode/js/index.js
@@ -1,3 +1,17 @@
-console.clear();
-
 const bodyElement = document.querySelector('[data-js="body"]');
+const buttonOn = document.querySelector('[data-js="dark-mode-button"]');
+const buttonOff = document.querySelector('[data-js="light-mode-button"]');
+const buttonToggle = document.querySelector('[data-js="toggle-button]');
+buttonOn.addEventListener('mouseup', () => bodyElement.className = 'dark');
+buttonOff.addEventListener('mouseup', () => bodyElement.className = '');
+
+function toggleDarkness(){
+    if (bodyElement.className === 'dark') {
+        bodyElement.className = '';
+    } else {
+        bodyElement.className = 'dark';
+    }
+}
+
+buttonToggle.addEventListener('mouseup', () => toggleDarkness());
+
