c9acc6a6e4f7202a4cc9cf9bd851296650121308
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-05-30 14:36:39 +0200

    added division and multiply. division to be connected to button

--- 0007-jsDOM/js-dom-and-events_simple-calculator/js/index.js
+++ 0007-jsDOM/js-dom-and-events_simple-calculator/js/index.js
@@ -35,14 +35,29 @@ Step 1: Select each button for updating `operand1` by its `data-js` attribute.
 Step 2: Add event listeners to update `operand1` based on the button clicked. Log the updated value to the console.
 */
 
-// --v-- write your code here --v--
 const buttonIncrease1 = document.querySelector('[data-js="increase-by-one"]');
 const buttonIncrease5 = document.querySelector('[data-js="increase-by-five"]');
+const buttonDecrease1 = document.querySelector('[data-js="decrease-by-one"]');
+const buttonDecrease5 = document.querySelector('[data-js="decrease-by-five"]');
+const buttonMultiplyOperand = document.querySelector('[data-js="multiply-by-two"]');
 
-function variableChanger(amount = 1, operation = 'add') {
-  operation = 'add' ? operand1 += amount : operand2 -= amount;
+function variableChanger(amount = 1, operation = "add") {
+  if(operation === 'add'){
+    operand1 += amount;
+  } else if (operation === 'substract') {
+    operand1 -= amount;
+  } else if (operation === 'multiply') {
+      operand1 *= amount;
+  }
+switch (operation) {
+    case ('divide'): operand1 /= amount; break;
+}
   console.log(operand1);
 }
 buttonIncrease1.addEventListener("mouseup", () => variableChanger());
-buttonIncrease5.addEventListener("mouseup", () => variableChanger(5));
-// --^-- write your code here --^--
+buttonIncrease5.addEventListener("mouseup", () =>
+  variableChanger(5, "add")
+);
+buttonDecrease1.addEventListener("mouseup", () => variableChanger(1, "substract"));
+buttonDecrease5.addEventListener('mouseup', () => variableChanger(5,'substract'));
+buttonMultiplyOperand.addEventListener('mouseup', () => variableChanger(2,'multiply'));
