5cb237e4306e0ffe61d4680d7040e39958c37f32
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-05-30 10:30:17 +0200

    changed all buttons to use classlist instead className to change dark mode

--- 0007-jsDOM/js-dom-and-events_dark-mode/js/index.js
+++ 0007-jsDOM/js-dom-and-events_dark-mode/js/index.js
@@ -2,8 +2,8 @@ const bodyElement = document.querySelector('[data-js="body"]');
 const buttonOn = document.querySelector('[data-js="dark-mode-button"]');
 const buttonOff = document.querySelector('[data-js="light-mode-button"]');
 
-buttonOn.addEventListener("mouseup", () => (bodyElement.className = "dark"));
-buttonOff.addEventListener("mouseup", () => (bodyElement.className = ""));
+buttonOn.addEventListener("mouseup", () => (bodyElement.classList.add('dark')));
+buttonOff.addEventListener("mouseup", () => (bodyElement.classList.remove('dark')));
 
 const buttonToggle = document.querySelector('[data-js="toggle-button"]');
-buttonToggle.addEventListener("mouseup", () => bodyElement.className = 'dark');
+buttonToggle.addEventListener("mouseup", () => bodyElement.classList.toggle('dark'));
