95fe4977e8c638a80972c2860b52e255c2aaaf7c
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-05-30 10:21:01 +0200

    toggle button code works for turning on dark

--- 0007-jsDOM/js-dom-and-events_dark-mode/js/index.js
+++ 0007-jsDOM/js-dom-and-events_dark-mode/js/index.js
@@ -2,9 +2,8 @@ const bodyElement = document.querySelector('[data-js="body"]');
 const buttonOn = document.querySelector('[data-js="dark-mode-button"]');
 const buttonOff = document.querySelector('[data-js="light-mode-button"]');
 
-buttonOn.addEventListener('mouseup', () => bodyElement.className = 'dark');
-buttonOff.addEventListener('mouseup', () => bodyElement.className = '');
+buttonOn.addEventListener("mouseup", () => (bodyElement.className = "dark"));
+buttonOff.addEventListener("mouseup", () => (bodyElement.className = ""));
 
-
-
-
+const buttonToggle = document.querySelector('[data-js="toggle-button"]');
+buttonToggle.addEventListener("mouseup", () => bodyElement.className = 'dark');
