e83970f66926c48858ec529d4adf963aeaf2792d
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-05-30 21:16:04 +0200

    part one done

--- 0007-jsDOM/0008-js-Conditions/js-conditions-and-booleans_weather-check/js/index.js
+++ 0007-jsDOM/0008-js-Conditions/js-conditions-and-booleans_weather-check/js/index.js
@@ -1,5 +1,19 @@
 // Change this value to test different "weather" conditions.
-const weather = "rainy"
-
+const weather = "rainy";
+let reply;
+switch (weather) {
+  case "rainy":
+    reply = "Brass monkey weather";
+    break;
+  case "sunny":
+    reply = "Praise Ra";
+    break;
+  case "windy":
+    reply = "Let us go surfing";
+    break;
+  default:
+    reply = "Weather always exists";
+}
+console.log(reply);
 // Adjust the "temperature" value to trigger different messages (for Part 2 only).
-const temperature = 22; 
+const temperature = 22;
