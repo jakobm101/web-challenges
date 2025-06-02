9cccd9e2dfd32cf6803ec380cef31c7ebdc71a69
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-06-02 17:27:10 +0200

    all done

--- 0010-js-functions/js-functions_greeting-page/js/index.js
+++ 0010-js-functions/js-functions_greeting-page/js/index.js
@@ -23,11 +23,40 @@ Update the content and style of the page based on the current day and time.
 const display = document.querySelector('[data-js="display"]');
 
 function getGreeting() {
-  // Code here
+  let hour = new Date().getHours();
+  hour = 22;
+  let greeting = "Hello";
+  switch (true) {
+    case 6 < hour && hour < 13:
+      greeting = "Good Morning";
+      break;
+    case 13 < hour && hour < 19:
+      greeting = "Good Afternoon";
+      break;
+    case 19 < hour && hour < 23:
+      greeting = "Good Evening";
+      break;
+    case 22 < hour && hour < 25:
+    case 0 < hour && hour < 6:
+      greeting = "Good Night";
+      break;
+  }
+  console.log(greeting);
 }
 
 function getDayColor() {
-  // Code here
+  let date = new Date().getDay();
+  console.log({ date });
+  //  - Monday: "darkgray"
+  // - Tuesday - Friday: "lightblue"
+  //  - Saturday - Sunday: "hotpink"
+  if (date === 1) {
+    return "darkgray";
+  } else if (date < 6) {
+    return "lightblue";
+  } else {
+    return "hotpink";
+  }
 }
 
 display.textContent = getGreeting();
