c986ff93fc6c9de1d646cfcbcff720f2a1f9baa5
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-06-02 20:05:18 +0200

    done

---
+++ 0010-js-functions/js-functions_progress-bar/js/index.js
@@ -0,0 +1,25 @@
+console.clear();
+
+const progressBar = document.querySelector('[data-js="progress-bar"]');
+
+function calculateScrollPercentage() {
+  /**
+  - `window.scrollY`: The Y position of the window on the total webpage.
+  - `window.innerHeight`: The height of the visible part of the window.
+  - `document.body.clientHeight`: The total height of the webpage.
+*/
+  progressBarUpdate = `${
+    window.scrollY / (document.body.clientHeight - window.innerHeight) *100
+  }%`;
+  progressBar.style.width = progressBarUpdate;
+  console.log(progressBarUpdate);
+}
+
+// checking values
+function logger() {
+  console.log(
+    `${window.scrollY},${window.innerHeight},${document.body.clientHeight}`
+  );
+}
+//  document.addEventListener("scroll", logger);
+document.addEventListener("scroll", calculateScrollPercentage);
