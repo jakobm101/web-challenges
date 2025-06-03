55a245ed56ceb9900e86e288f448f6a242277e8c
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-06-03 20:58:55 +0200

    git stuff

--- 0012-js-strings/js-inputs-and-strings_change-box-appearance/js/index.js
+++ 0012-js-strings/js-inputs-and-strings_change-box-appearance/js/index.js
@@ -1,11 +1,16 @@
 console.clear();
 
-const box = document.querySelector('[data-js=box]')
+const box = document.querySelector("[data-js=box]");
 // box.style.background = 'green'
-const inputColor = document.querySelector('[data-js=input-color]')
-console.log(inputColor);
-
-inputColor.addEventListener('mouseup', () => {
-    box.style.background = `hsl(${inputColor.value}, 60%, 70%)`
-})
-console.log('yo');
+const inputColor = document.querySelector("[data-js=input-color]");
+const inputRadius = document.querySelector("[data-js=input-radius]");
+const inputRotation = document.querySelector("[data-js=input-rotation]");
+inputColor.addEventListener("input", () => {
+  box.style.background = `hsl(${inputColor.value}, 60%, 70%)`;
+});
+inputRadius.addEventListener("input", () => {
+  box.style.borderRadius = `${inputRadius.value * 2}px`;
+});
+inputRotation.addEventListener("input", () => {
+  box.style.transform = `rotate(${inputRotation.value}deg)`;
+});
