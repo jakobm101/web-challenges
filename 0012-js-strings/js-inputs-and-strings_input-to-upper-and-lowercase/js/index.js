972bd840fb2799f766b3a3ab365723b6578e2c10
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-06-03 15:45:07 +0200

    listening to lowercase button

--- 0012-js-strings/js-inputs-and-strings_input-to-upper-and-lowercase/js/index.js
+++ 0012-js-strings/js-inputs-and-strings_input-to-upper-and-lowercase/js/index.js
@@ -7,3 +7,6 @@ const lowercaseButton = document.querySelector('[data-js=button-lowercase]')
 uppercaseButton.addEventListener("click", () => {
   input.value = input.value.toUpperCase();
 });
+lowercaseButton.addEventListener('click', () => {
+  input.value = input.value.toLowerCase();
+})
