95246c6c02f0c882707ffd5d63f45cd23430e7c1
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-06-03 16:56:18 +0200

    color slider working

--- 0012-js-strings/js-inputs-and-strings_change-box-appearance/js/index.js
+++ 0012-js-strings/js-inputs-and-strings_change-box-appearance/js/index.js
@@ -1,1 +1,11 @@
 console.clear();
+
+const box = document.querySelector('[data-js=box]')
+// box.style.background = 'green'
+const inputColor = document.querySelector('[data-js=input-color]')
+console.log(inputColor);
+
+inputColor.addEventListener('mouseup', () => {
+    box.style.background = `hsl(${inputColor.value}, 60%, 70%)`
+})
+console.log('yo');
