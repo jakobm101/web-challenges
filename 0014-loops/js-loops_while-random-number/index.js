50829bcf7b9275f1fd7f43eaa819ba878556cad1
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-06-06 19:18:37 +0200

    faking git push

--- 0014-loops/js-loops_while-random-number/index.js
+++ 0014-loops/js-loops_while-random-number/index.js
@@ -4,12 +4,7 @@ let number = 0;
 let count = 0;
 
 // --v-- write or modify code below this line --v--
-while(number<=0.9){
-  count++
-  console.log({number});
-  number = Math.random()
-  
-}
+
 // --^-- write or modify code above this line --^--
 
 console.log(
