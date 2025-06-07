5da6c0568d04aeaf3ce005c7e9b07d915ce8be2c
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-06-07 17:50:04 +0200

    task 2 done

--- 0014-loops/js-loops_stars/js/index.js
+++ 0014-loops/js-loops_stars/js/index.js
@@ -2,15 +2,16 @@ console.clear();
 
 const starContainer = document.querySelector('[data-js="star-container"]');
 
-function renderStars() {
+function renderStars(filledStars = 3) {
   // Reset the star container before re-rendering stars
   starContainer.innerHTML = "";
   for (let i = 0; i < 5; i++) {
     const star = document.createElement("img");
-    star.setAttribute("src", "assets/star-empty.svg");
+    filledStars <= i ? star.setAttribute("src", "assets/star-empty.svg") : star.setAttribute('src', 'assets/star-filled.svg')
     console.log(star);
 
     starContainer.append(star);
   }
+
 }
-renderStars();
+renderStars(4);
