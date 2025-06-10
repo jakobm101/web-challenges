4235346d00c2baee5610640aa0b2a8f0d8a7914b
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-06-10 19:55:05 +0200

    all done

--- 0014-arraymethods/js-array-methods_animal-strings/js/index.js
+++ 0014-arraymethods/js-array-methods_animal-strings/js/index.js
@@ -67,7 +67,9 @@ console.log(everyAnimalHasMoreThanTwoLetters);
 // Option 1: Concatenate all characters with `reduce()` and check for the `length` property of the result.
 // Option 2: Use `map()` to create an array with the length values of all strings,
 // 				then sum them up with `reduce()` (keyword: method chaining)
-const sumOfAllAnimalCharacters = null;
+const sumOfAllAnimalCharacters = animalStrings.reduce((accu , ani) => accu += ani)
+console.log(sumOfAllAnimalCharacters);
+
 
 export {
   hippoExists,
