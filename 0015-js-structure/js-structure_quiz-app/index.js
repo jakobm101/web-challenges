a1f113919e7881911aa9edd692cb7cb0e9c97270
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-06-10 17:03:29 +0200

    app component created

--- 0015-js-structure/js-structure_quiz-app/index.js
+++ 0015-js-structure/js-structure_quiz-app/index.js
@@ -1,18 +1,7 @@
-import cards from './components/CardList/CardArray.js'
-import Card from './components/Card/Card.js'
 import CardList from './components/CardList/CardList.js'
 import Header from './components/Header/Header.js'
 import Form from './components/Form/Form.js'
-import Bookmark from './components/Bookmark/Bookmark.js'
-
-function App() {
-  const app = document.createElement("main");
-  app.classList.add("app");
-  app.append(Header(), Form(), CardList());
-
-  return app;
-}
-
+import App from './components/App/App.js'
 // The render function renders the app to the DOM
 function render() {
   const root = document.querySelector("#root");
