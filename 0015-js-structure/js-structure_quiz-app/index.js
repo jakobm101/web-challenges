aab9c4e313a0dfcb55eac7bce853d501624d21ba
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-06-10 17:04:00 +0200

    cleaned up

--- 0015-js-structure/js-structure_quiz-app/index.js
+++ 0015-js-structure/js-structure_quiz-app/index.js
@@ -1,7 +1,5 @@
-import CardList from './components/CardList/CardList.js'
-import Header from './components/Header/Header.js'
-import Form from './components/Form/Form.js'
 import App from './components/App/App.js'
+
 // The render function renders the app to the DOM
 function render() {
   const root = document.querySelector("#root");
