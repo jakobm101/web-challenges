87a02b434ae0b6cd8bd7c82917fde35de1ec7aaf
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-06-17 11:36:57 +0200

    hello art

---
+++ 0020-react/react-basics_hello-world-article/src/Components/HelloWorldArticle/HelloWorldArticle.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function HelloWorldArticle() {
+  return (
+    <article>
+      <h1>Hello World Article</h1>
+      <p>This is what this is</p>
+    </article>
+  );
+}
