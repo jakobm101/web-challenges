2dde61d467f08ef6f261dc52b2a39d49555e6169
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-06-18 11:11:30 +0200

    done

---
+++ 0022-react-nesting/react-nesting_fragments/src/components/Boxes/Boxes.jsx
@@ -0,0 +1,9 @@
+import Box from '../Box.jsx'
+
+export default function Boxes ({children}) {
+    return (<>
+    <Box color={'Chartreuse'}></Box>
+    <Box color={'Chartreuse'}></Box>
+    <Box color={'Chartreuse'}></Box>
+    </>)
+}
