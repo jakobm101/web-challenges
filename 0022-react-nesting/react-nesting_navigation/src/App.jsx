d2c2c954227ffe625cb8800d96016c75addf572f
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-06-18 11:44:02 +0200

    nav compo works

--- 0022-react-nesting/react-nesting_navigation/src/App.jsx
+++ 0022-react-nesting/react-nesting_navigation/src/App.jsx
@@ -1,4 +1,5 @@
 import "./styles.css";
+import Navigation from "./components/Navigation";
 import Header from "./components/Header/Header";
 
 import avatar from "./img/avatar.jpg";
@@ -11,7 +12,7 @@ export default function App() {
         <a href="#">
           <img className="round-image" src={logo} alt="logo" />
         </a>
-        <nav>
+        <Navigation>
           <a className="navigation__link" href="#home">
             Home
           </a>
@@ -21,7 +22,7 @@ export default function App() {
           <a className="navigation__link" href="#impressum">
             Impressum
           </a>
-        </nav>
+        </Navigation>
         <button
           type="button"
           onClick={() => console.log("I could toggle a profile!")}
